3f76c02eed9b272eb5ad6f5002d7a0d3cf6dea04
Marios <mariossstefanis@gmail.com>
2026-07-18 14:51:18 +0300

    the core of the db/flavio check and change anything if it is required beacuse i did not have the domain so i did it as i thought it could be,there are a breif readme at discord and commentrs through the code

---
+++ Application/Abstractions/ICountryRepository.cs
@@ -0,0 +1,10 @@
+﻿using Domain;
+
+
+namespace Application.Abstractions;
+
+public interface ICountryRepository
+{
+    Task<Country?> GetByCodeAsync(string twoLetterCode,CancellationToken ct = default);
+    Task AddIfNotExistsAsync(Country country,CancellationToken ct = default);
+}
---
+++ Application/Abstractions/IIpRepository.cs
@@ -0,0 +1,11 @@
+﻿using Domain;
+
+namespace Application.Abstractions;
+    public interface IIpRepository
+    {
+        Task<Ip?> GetByAddressAsync (string address , CancellationToken ct = default); //returns ip used in task1 ,look at db before IP2C
+        Task <Ip> AddAsync (Ip ip, CancellationToken ct = default); //adds ip to the database);
+        Task SaveChangesAsync(CancellationToken ct = default); //saves changes to the database
+        Task<IReadOnlyList<Ip>> GetBatchAsync (int skip, int take, CancellationToken ct = default); //returns a batch of ips, the job part for the feature
+        Task<IReadOnlyList<ReportRow>> GetReportAsync(string[]? countryCodes, CancellationToken ct = default); // report for the countries, readonly because we do not want to modify data
+    }
